4a08785175cfa0df0d89051dfd7d10d8f5b07694
Ondrej Charvat <ondrej.charvat@worldline.com>
2025-01-02 08:53:27 +0100

    KPB-002 | navbar

--- src/app/core/navigation/navigation.component.ts
+++ src/app/core/navigation/navigation.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule, IconDefinition } from '@fortawesome/angular-fontawesome';
+import { faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons';
+import {NgFor, NgIf} from '@angular/common';
+
+export interface NavItem {
+  link: string,
+  label: string
+}
+
+export interface SocialMediaItem {
+  link: string,
+  icon: IconDefinition
+}
 
 @Component({
   selector: 'app-navigation',
-  imports: [],
+  imports: [FontAwesomeModule, NgFor, NgIf],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.sass'
 })
 export class NavigationComponent {
+  readonly faInstagram = faInstagram;
+  readonly faFacebook = faFacebook;
 
+  readonly availableSections: NavItem[]= [
+    {link: '#sectionHome', label: 'domů'},
+    {link: '#sectionServices', label: 'služby'},
+    {link: 'sectionAboutMe', label: 'o mě'},
+    {link: '#sectionGallery', label: 'galerie'},
+    {link: '#sectionContact', label: 'kontakt'}
+  ]
+
+  readonly availableSocialMedia: SocialMediaItem[] = [
+    {link: '#', icon: faInstagram},
+    {link: '#', icon: faFacebook}
+  ]
 }
