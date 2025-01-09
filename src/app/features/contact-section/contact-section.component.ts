a574aead650c6d9502ff19737fac925859afcb51
Ondrej Charvat <ondrej.charvat@worldline.com>
2025-01-09 15:50:12 +0100

    KPB-009 | gallery section

--- src/app/features/contact-section/contact-section.component.ts
+++ src/app/features/contact-section/contact-section.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import {faUser, faHouse, faPhone, faAt} from '@fortawesome/free-solid-svg-icons';
+import { faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-contact-section',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './contact-section.component.html',
   styleUrl: './contact-section.component.sass'
 })
 export class ContactSectionComponent {
+  readonly faUser = faUser;
+  readonly faHouse = faHouse;
+  readonly faPhone = faPhone;
+  readonly faAt = faAt;
+  readonly faInstagram = faInstagram;
+  readonly faFacebook = faFacebook;
 
 }
