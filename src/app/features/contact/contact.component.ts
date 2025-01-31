3df812ec7d8a90add5278b116692e4d5461813c3
Ondrej Charvat <ondrej.charvat@worldline.com>
2025-01-31 22:44:37 +0100

    KPB-023 | contact

--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import {VisibleClassDirective} from '../../shared/directives/visible-class.directive';
+import {faFacebook, faInstagram} from '@fortawesome/free-brands-svg-icons';
+import {faAt, faHouse, faPhone, faUser} from '@fortawesome/free-solid-svg-icons';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [VisibleClassDirective, FaIconComponent],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.sass'
+  styleUrl: './contact.component.sass',
+  standalone: true
 })
 export class ContactComponent {
 
+  readonly faFacebook = faFacebook;
+  readonly faPhone = faPhone;
+  readonly faAt = faAt;
+  readonly faUser = faUser;
+  readonly faHouse = faHouse;
+  readonly faInstagram = faInstagram;
 }
