8645263b95f078949976210bd8b7d7bda0500db8
Ondrej Charvat <ondrej.charvat@worldline.com>
2025-02-05 22:54:09 +0100

    KPB-030 | about

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
-import {Observable} from 'rxjs';
-import {PersonalInfo} from '../../shared/models/models';
-import {DataService} from '../../shared/services/data.service';
-import {AsyncPipe, NgFor, NgIf} from '@angular/common';
-import {VisibleClassDirective} from '../../shared/directives/visible-class.directive';
+import { Observable } from 'rxjs';
+import { PersonalInfo } from '../../shared/models/models';
+import { DataService } from '../../shared/services/data.service';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+import { VisibleClassDirective } from '../../shared/directives/visible-class.directive';
+import { faAnglesRight, faAnglesLeft } from '@fortawesome/free-solid-svg-icons';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-about',
-  imports: [NgIf, NgFor, AsyncPipe, VisibleClassDirective],
+  imports: [NgIf, NgFor, AsyncPipe, VisibleClassDirective, FaIconComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.sass',
   standalone: true
 })
 export class AboutComponent {
   personalInfo$: Observable<PersonalInfo> | null = null;
+
+  readonly faAnglesRight = faAnglesRight;
+  readonly faAnglesLeft = faAnglesLeft;
 
   constructor(readonly dataService: DataService) {
     this.personalInfo$ = dataService.getPersonalInfo();
