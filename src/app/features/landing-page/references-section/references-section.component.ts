0a43be9fd1f862daf32b565e83f1ba64da1e4b48
Ondrej Charvat <ondrej.charvat@worldline.com>
2025-02-07 20:14:04 +0100

    KPB-031 | references

--- src/app/features/landing-page/references-section/references-section.component.ts
+++ src/app/features/landing-page/references-section/references-section.component.ts
@@ -1,11 +1,12 @@
-import {Component} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {faQuoteLeft} from '@fortawesome/free-solid-svg-icons';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { VisibleClassDirective } from '../../../shared/directives/visible-class.directive';
 
 @Component({
   selector: 'app-references-section',
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule, VisibleClassDirective],
   templateUrl: './references-section.component.html',
   styleUrl: './references-section.component.sass',
   standalone: true
